d865fb04cd87e5e3b85f4298b6f1e6bc9f844675
Galieva Iluza <galievailyu@yandex.ru>
2024-09-09 10:44:51 +0300

    add models

---
+++ backend/models.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum, Text, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from .database import Base
+
+class Employee(Base):
+    __tablename__ = "employee"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
+
+class Organization(Base):
+    __tablename__ = "organization"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    description = Column(Text)
+    type = Column(Enum('IE', 'LLC', 'JSC', name='organization_type'))
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
+
+class Tender(Base):
+    __tablename__ = "tender"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    description = Column(Text)
+    organization_id = Column(Integer, ForeignKey("organization.id"))
+    status = Column(String(50), default="CREATED")
+    version = Column(Integer, default=1)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
+
+class Bid(Base):
+    __tablename__ = "bid"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    description = Column(Text)
+    tender_id = Column(Integer, ForeignKey("tender.id"))
+    status = Column(String(50), default="CREATED")
+    version = Column(Integer, default=1)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
